ee5e98b2cd342c5c7db85266dc996cebf4a73a77
Ramazan Nazkey <nazkeyramazan@gmail.com>
2022-03-26 13:06:31 +0600

    data fetched from firebase

--- 2week/lesson1/src/App.js
+++ 2week/lesson1/src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
+import React, {useEffect, useState} from 'react'
 import CommentBlock from './components/CommentBlock';
+
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+      fetch("https://kdwed-f1dd2-default-rtdb.europe-west1.firebasedatabase.app/comments.json")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setData(result);
+          },
+        )
+    }, [])
   return (
-    <>
-      <CommentBlock />
-      <CommentBlock />
-      <CommentBlock />
-    </>
+    <div className="App">
+        Comments Block
+        {data.map((comment, index) =>
+            <CommentBlock comment={comment} key={index} />
+        )}
+    </div>
   );
 }
 
--- 2week/lesson1/src/components/CommentBlock.js
+++ 2week/lesson1/src/components/CommentBlock.js
@@ -1,13 +1,15 @@
-function CommentBlock() {
+function CommentBlock({comment}) {
+
+    const date = new Date(comment.created);
     return (
         <div className="comment_block">
             <div className="top">
-                <img className="avatar" src="https://cdn.promodj.com/afs/ff76b55b804c8b9cbbed5a68b82609a111%3Aresize%3A440x732%3Asame%3A37cac4" alt="" />
-                <span className="name">Vasya Pupkin</span>
-                <span className="date">11:00 26.03.2022</span>
+                <img className="avatar" src={comment.author.avatarUrl} alt="" />
+                <span className="name">{comment.author.name}</span>
+                <span className="date">{date.toLocaleString()}</span>
             </div>
             <div className="text">
-                Hot Stuff!
+                {comment.text}
             </div>
         </div>
     )
