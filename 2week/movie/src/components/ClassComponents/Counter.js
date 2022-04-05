f5a7c704fa083257f9021ca8cc2c8f0b2fc091a0
Ramazan Nazkey <nazkeyramazan@gmail.com>
2022-04-05 12:53:59 +0600

    clearInterval added

--- 2week/movie/src/components/ClassComponents/Counter.js
+++ 2week/movie/src/components/ClassComponents/Counter.js
@@ -4,20 +4,41 @@ import Button from '@mui/material/Button';
 class Counter extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {counter: 0}
+      this.state = {counter: 20}
+    }
+    interval = null;
+    startCountDown(){
+        clearInterval(this.interval);
+       this.interval = setInterval(()=>{
+        console.log(this.state.counter)
+
+            if(this.state.counter>0){
+                this.setState({counter: this.state.counter-1});
+            }else{
+                clearInterval(this.interval)
+            }
+        },1000)
     }
     increaseCounter(){
+        clearInterval(this.interval);
         this.setState({counter: this.state.counter+1});
     }
     decreaseCounter(){
+        clearInterval(this.interval);
         this.setState({counter: this.state.counter-1});
+    }
+    componentWillUnmount(){
+        console.log('counter ' + this.state.counter)
+        
+        clearInterval(this.interval);
     }
     render() {
       return (
-          <>
+        <>
             <div>{this.state.counter}</div>
             <Button onClick={() => this.increaseCounter()}> Increase </Button>
             <Button onClick={() => this.decreaseCounter()}> Decrease </Button>
+            <Button onClick={() => this.startCountDown()}> Auto Decrease </Button>
         </>
 
       );
