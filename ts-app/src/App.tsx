fa9c1a527e882a212c4e8acbe3930b2c0e50c01e
Farabi Askar <askar.farabi12@gmail.com>
2022-05-03 20:01:49 +0600

    added button to close order modal

--- ts-app/src/App.tsx
+++ ts-app/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import {ShopComponent} from './components/shopComponent/ShopComponent'
+import { ShopComponent } from './components/shopComponent/ShopComponent'
 import ToDoComponent from './components/toDoComponent/toDoComponent';
-import {DndProvider} from "react-dnd";
-import {HTML5Backend} from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 function App() {
     return (
         // <ShopComponent/>
-        <div style={{margin:'auto'}}>
+        <div style={{ margin: 'auto' }}>
             <DndProvider backend={HTML5Backend}>
-                <ToDoComponent />
+                <ShopComponent />
             </DndProvider>
         </div>
     )
