7e1629e96a8e3e5996faf205a7ded98bc453512b
Ramazan Nazkey <nazkeyramazan@gmail.com>
2022-05-14 12:16:16 +0600

    empty app with new component

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { CodeBlockComponent } from './components/code-block/code-block.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CodeBlockComponent
   ],
   imports: [
     BrowserModule
